9c4a698df3d167aca42c62b3ae28113f63f2c23e
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-10-30 17:47:24 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,13 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
         pass
+        print(json.loads(self.get_response_body()))
+        return json.loads(self.get_response_body())
+
+data = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+data.load_json()
